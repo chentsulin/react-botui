8f22897d3a1932d77b0cedaa7e4ca1d5515f004c
chentsulin <chentsulin@gmail.com>
2018-10-10 00:44:21 +0800

    remove git submodule

---
+++ examples/hello-world/src/App.js
@@ -0,0 +1,61 @@
+import React, { Component } from 'react';
+import BotUI from '@chentsulin/react-botui';
+
+import 'botui/build/botui.min.css';
+import 'botui/build/botui-theme-default.css';
+import './App.css';
+
+class App extends Component {
+  state = {
+    messages: [
+      {
+        human: true,
+        content: 'hello',
+      },
+      {
+        content: 'world',
+      },
+    ],
+  };
+
+  handleAction = res => {
+    this.setState({
+      messages: [
+        ...this.state.messages,
+        {
+          human: true,
+          content: res.value,
+        },
+        {
+          content: `Received ${res.value}!`,
+        },
+      ],
+    });
+  };
+
+  handleReset = () => {
+    this.setState({
+      messages: [],
+    });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <BotUI
+          messages={this.state.messages}
+          action={{
+            type: 'text',
+            action: {
+              placeholder: 'Enter your text here',
+            },
+          }}
+          onAction={this.handleAction}
+        />
+        <button onClick={this.handleReset}>Reset</button>
+      </div>
+    );
+  }
+}
+
+export default App;
